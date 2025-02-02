5334df115df32958b5d251f6da90de15bbc626d3
GROOGiepng <mitchelllilnoise@gmail.com>
2025-02-01 20:53:20 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // Function to handle button click events
 function selectOption(option) {
     // Check which option was clicked
-    if (option === 'yes') {
+    if (option === 'OMGGG YES GREG') {
         // Flash rainbow colors
         flashRainbowColors(function() {
             document.getElementById('question').style.display = 'none'; // Hide the question
@@ -12,13 +12,13 @@ function selectOption(option) {
     } else if (option === 'no') {
         // Change text on the "No" button to "You sure?"
         document.getElementById('no-button').innerText = 'You sure?'; 
-        // Increase font size of "Yes" button
-        var yesButton = document.getElementById('yes-button');
-        var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
+        // Increase font size of "OMGG YESS GREGG" button
+        var OMGGYESSGREGGButton = document.getElementById('OMGG YESS GREGG-button');
+        var currentFontSize = window.getComputedStyle(OMGGYESSGREGGButton).getPropertyValue('font-size');
         var newSize = parseFloat(currentFontSize) * 2; // Increase font size by  * 2px
-        yesButton.style.fontSize = newSize + 'px';
+        OMGGYESSGREGGButton.style.fontSize = newSize + 'px';
     } else {
-        // If neither "Yes" nor "No" was clicked, show an alert message
+        // If neither "OMGG YESS GREGG" nor "No" was clicked, show an alert message
         alert('Invalid option!');
     }
 }
